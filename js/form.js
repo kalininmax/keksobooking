558b1cfa7c3b9eddf5f64b4a4b8dccdfc3a92f32
Maxim Kalinin <kalinin.m.a@bk.ru>
2021-02-22 13:44:49 +0300

    Добавляет синхронизацию полей выбора времени заезда и выезда

--- js/form.js
+++ js/form.js
@@ -3,8 +3,18 @@ import { TYPES } from './data.js';
 const adForm = document.querySelector('.ad-form');
 const typeSelect = adForm.querySelector('#type');
 const priceInput = adForm.querySelector('#price');
+const timeInSelect = adForm.querySelector('#timein');
+const timeOutSelect = adForm.querySelector('#timeout');
 
 typeSelect.addEventListener('change', () => {
   priceInput.min = TYPES[typeSelect.value].minPrice;
   priceInput.placeholder = TYPES[typeSelect.value].minPrice;
+});
+
+timeInSelect.addEventListener('change', () => {
+  timeOutSelect.value = timeInSelect.value;
+});
+
+timeOutSelect.addEventListener('change', () => {
+  timeInSelect.value = timeOutSelect.value;
 });
