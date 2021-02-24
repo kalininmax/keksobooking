780a291b3a683153d438f4bb6ba4b6783e332197
Maxim Kalinin <kalinin.m.a@bk.ru>
2021-02-24 22:16:10 +0300

    Добавляет функции для отключения форм

--- js/form.js
+++ js/form.js
@@ -1,10 +1,12 @@
 import { TYPES } from './data.js';
 
 const adForm = document.querySelector('.ad-form');
+const adFormFieldsets = adForm.querySelectorAll('fieldset');
 const typeSelect = adForm.querySelector('#type');
 const priceInput = adForm.querySelector('#price');
 const timeInSelect = adForm.querySelector('#timein');
 const timeOutSelect = adForm.querySelector('#timeout');
+const filterForm = document.querySelector('.map__filters');
 
 typeSelect.addEventListener('change', () => {
   priceInput.min = TYPES[typeSelect.value].minPrice;
@@ -18,3 +20,20 @@ timeInSelect.addEventListener('change', () => {
 timeOutSelect.addEventListener('change', () => {
   timeInSelect.value = timeOutSelect.value;
 });
+
+const disableAdForm = () => {
+  adForm.classList.add('ad-form--disabled');
+  adFormFieldsets.forEach(fieldset => {
+    fieldset.setAttribute('disabled', 'disabled');
+  });
+};
+
+const disableFilterForm = () => {
+  filterForm.classList.add('map__filters--disabled');
+  for (let i = 0; i < filterForm.children.length; i++) {
+    filterForm.children[i].setAttribute('disabled', 'disabled');
+  }
+};
+
+disableAdForm();
+disableFilterForm();
