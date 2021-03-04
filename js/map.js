e1b3e51f0f67dcf974d26be5adce0bc850722a68
Maxim Kalinin <kalinin.m.a@bk.ru>
2021-03-04 23:24:30 +0300

    Исправляет пути к иконкам меток

--- js/map.js
+++ js/map.js
@@ -18,13 +18,13 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
 const pinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: './img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
   popupAnchor: [0, -10],
