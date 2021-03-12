fd00d3e73a98c193155b4423d9876109ebd9a065
Maxim Kalinin <kalinin.m.a@bk.ru>
2021-03-12 22:42:25 +0300

    Добавляет функции получения и отправки данных

--- js/data.js
+++ js/data.js
@@ -1,12 +1,31 @@
+const GET_DATA_URL = 'https://22.javascript.pages.academy/keksobooking/data';
+const SEND_DATA_URL = 'https://22.javascript.pages.academy/keksobooking';
+
 const getData = (onSuccess) => {
-  fetch('https://22.javascript.pages.academy/keksobooking/data')
+  fetch(GET_DATA_URL)
     .then((response) => response.json())
     .then((offers) => {
       onSuccess(offers);
-    })
-    .catch((a) => {
-      console.log(a)
     });
 };
 
-export { getData };
+const sendData = (onSuccess, onFail, body) => {
+  fetch(SEND_DATA_URL,
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+};
+
+export { getData, sendData };
