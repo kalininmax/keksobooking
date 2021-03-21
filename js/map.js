23be77dcdf1700ecc4e61d64fe520747f6e9e625
Maxim Kalinin <kalinin.m.a@bk.ru>
2021-03-21 13:13:38 +0300

    Включает формы при успешной загрузке данных

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 /* global L:readonly */
-import { setAddresInputValue } from './form.js';
+import { setAddresInputValue, disableAdForm, disableFilterForm } from './form.js';
 import { getAddress, showErrorMessage, debounce } from './util.js';
 import { createCard } from './card.js';
 import { getData } from './data.js';
@@ -89,6 +89,8 @@ const onSuccess = (data) => {
   offers = data.slice();
   renderOffers(offers.slice(0, MAX_OFFERS));
   filterForm.addEventListener('change', onFilterChange);
+  disableFilterForm();
+  disableAdForm();
 };
 
 const onFail = () => {
